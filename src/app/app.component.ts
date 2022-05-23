0723a0d40bce9814709adc868431ba7ef2dfdfb1
Mateusz Walęga <mateuszw.contact@gmail.com>
2022-05-23 20:50:10 +0200

    zmiany

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './task';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list-udemy';
+  taskName: string;
+  config: { [key: string]: string } | null = null;
+  tasks: Task[] = [
+    {
+      name: 'Siłownia',
+      deadline: '2020-01-02',
+      done: false,
+    },
+    {
+      name: 'Nauka Angulara',
+      deadline: '2020-01-03',
+      done: false,
+    },
+    {
+      name: 'Sprzątanie kuwety',
+      deadline: '2020-01-04',
+      done: false,
+    },
+  ];
+
+  constructor(){
+    this.config = {
+      title: 'Lista zadań',
+      footer:  '© Lista zadań. All rights reserved.',
+      date: new Date().toDateString()
+    };
+  }
+
+  clearTasks() {
+    this.tasks = [];
+  }
+
+  onKeyUp(event: KeyboardEvent){
+    const target = event.target as HTMLInputElement;
+    this.taskName = target.value;
+  }
+
+  createTask(){
+    const task: Task = {
+      name: this.taskName,
+      deadline: '2022-01-10',
+      done: false
+    };
+
+    this.tasks.push(task);
+  }
 }
