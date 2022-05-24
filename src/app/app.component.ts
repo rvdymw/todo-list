fa5e6fce5ed79c2f574e3774fe92a868ab92ed89
Mateusz Walęga <mateuszw.contact@gmail.com>
2022-05-24 20:17:09 +0200

    24.05

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,8 @@ import { Task } from './task';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  taskName = '';
+  taskDate = '';
   config: { [key: string]: string } | null = null;
   tasks: Task[] = [
     {
@@ -38,13 +40,15 @@ export class AppComponent {
     this.tasks = [];
   }
 
-  createTask(name: string, deadline: string){
+  createTask(){
     const task: Task = {
-      name,
-      deadline,
+      name: this.taskName,
+      deadline: this.taskDate,
       done: false
     };
 
     this.tasks.push(task);
+    this.taskName = '';
+    this.taskDate = ''
   }
 }
